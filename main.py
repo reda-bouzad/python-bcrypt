980cdfbfe6f7f9c05e69186dec827ae02813f38a
reda-bouzad <redabouzadfst@gmail.com>
2023-10-25 22:46:27 +0100

    v2

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import bcrypt
 
+# example password 
 password = "mypassword"
 
+# converting password to array of bytes 
 bytes = password.encode('utf-8')
 
+# generating the salt 
 salt = bcrypt.gensalt()
 
+# Hashing the password 
 hashedPassword = bcrypt.hashpw(bytes, salt)
 
+# Taking user entered password  
+userPassword =  'password000'
+
+# encoding user password 
+userBytes = userPassword.encode('utf-8') 
+  
+# checking password 
+result = bcrypt.checkpw(userBytes, hashedPassword) 
+
+
 print(hashedPassword)
+print(result)
